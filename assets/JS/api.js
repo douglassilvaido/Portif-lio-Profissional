7589823c2aa6262410b8de2f74d9bca23b44a1ef
Douglassilva <117125295+douglassilvaido@users.noreply.github.com>
2024-01-08 15:30:21 -0300

    Atualizando profile.json

--- assets/JS/api.js
+++ assets/JS/api.js
@@ -1,6 +1,7 @@
 
 async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/douglassilvaido/Portif-lio-Profissional/main/Data/profile.json'
-    const fetching = await fetch(url)
-    return await fetching.json()
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
 }
